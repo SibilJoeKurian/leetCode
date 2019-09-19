b45eab0517f60df88a8c822768ffcf709ec00bd5
SibilJoeKurian <Sibil.kurian@gmail.com>
2019-09-18 20:29:41 -0400

    First Problem easy

--- LeetCode/src/test/Solution1.java
+++ LeetCode/src/test/Solution1.java
@@ -1,5 +1,7 @@
 package test;
-
+//Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+//
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
 class Solution1 {
 	public int[] twoSum(int[] nums, int target) {
 		int temp;
