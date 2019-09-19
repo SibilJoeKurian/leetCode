35f8b2b1e0c105bfd5049eaf8273660422a5011c
SibilJoeKurian <Sibil.kurian@gmail.com>
2019-09-18 20:25:36 -0400

    First program

---
+++ LeetCode/src/test/Solution1.java
@@ -0,0 +1,31 @@
+package test;
+
+class Solution1 {
+	public int[] twoSum(int[] nums, int target) {
+		int temp;
+		int[] twoSums = new int[2];
+		for (int i = 0; i < nums.length; i++) {
+			if ((i + 1) < nums.length) {
+				for (int j = i + 1; j < nums.length; j++) {
+					temp = nums[i];
+					int sum = temp + nums[j];
+					if (sum == target) {
+						twoSums[0] = i;
+						twoSums[1] = j;
+						//System.out.println(twoSums[0] + " " + twoSums[1]);
+						//System.out.println(i + " " + j);
+						return twoSums;
+					}
+				}
+			}
+		}
+		return twoSums;
+	}
+
+	public static void main(String[] args) {
+		Solution1 s = new Solution1();
+		int a[] = { 3, 2, 4 };
+		int b[] = s.twoSum(a, 6);
+		System.out.println(b[0] + " " + b[1]);
+	}
+}
