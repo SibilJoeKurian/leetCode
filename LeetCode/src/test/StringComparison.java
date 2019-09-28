747d35721b064f91eccbc6cb0b4315edb3b2785d
SibilJoeKurian <Sibil.kurian@gmail.com>
2019-09-28 16:11:53 -0400

    Changed the code with linear complexity

--- LeetCode/src/test/StringComparison.java
+++ LeetCode/src/test/StringComparison.java
@@ -1,81 +1,44 @@
 package test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.TreeMap;
 
 public class StringComparison {
-	static HashMap<Character, Integer> mapStr1 = new HashMap<>();
-	static HashMap<Character, Integer> mapStr2 = new HashMap<>();
-	static int left;
-	static int right;
-
-	public static boolean contains(String str1, String str2) {
-
-		boolean isContains = false;
-		mapString(str1, mapStr1);
-		mapString(str2, mapStr2);
-
-		boolean result = checkHashMap();
-		System.out.println("Resykt" + result);
-		return isContains;
-	}
-
-	private static boolean checkHashMap() {
-		// TODO Auto-generated method stub
-		boolean checkValue = false;
-		boolean checkFlagCounter;
-		if (mapStr1.size() < mapStr2.size())// check size of str1 is less than str2
-			return checkValue;
-		else {
-			for (Object key : mapStr2.keySet()) {
-				Integer charCountStr2 = mapStr2.get(key);
-				if (mapStr1.containsKey(key)) // check str1 contains the char
-				{
-					Integer charCountStr1 = mapStr1.get(key);
-					if (charCountStr1 < charCountStr2) // check str1 size with str2
-						return checkValue;
-				} else
-					return checkValue;
-			}
-			checkValue = true;
-		}
-
-		return checkValue;
-	}
-
-	public static void mapString(String str, HashMap<Character, Integer> map) {
-		for (int i = 0; i < str.length(); i++) {
-			if (map.containsKey(str.charAt(i))) {
-				int count = map.get(str.charAt(i));
-				map.put(str.charAt(i), ++count);
-			} else
-				map.put(str.charAt(i), 1);
-		}
-	}
-
-	public static void displayMap() {
-		System.out.println(mapStr1);
-		System.out.println(mapStr2);
-	}
-
-	public static String minWindow(String mainString, String t) {
-		String minString = "";
-		left = 0;
-		right = 0;
-		
-		return minString;
-	}
-
-	private static boolean makeString(int left2, int right2) {
-		// TODO Auto-generated method stub
-		
-		return false;
-	}
 
 	public static void main(String[] args) {
-		String s = "ADOECODEBANC";
-		String t = "ANQ";
-		minWindow(s, t);
-		contains(s, t);
-		displayMap();
+
+		String s1 = "ADOBECODEBANC";
+		String s = "AAB";
+		String t = "AAAA";
+		//compare(s, t);
+		StringComparison.compare("ABC", "BC");
+	}
+
+	public static boolean compare(String s, String t) {
+		// TODO Auto-generated method stub
+		boolean result = true;
+		HashMap<Character, Integer> sMap = new HashMap<>();
+		HashMap<Character, Integer> tMap = new HashMap<>();
+		for (int i = 0; i < s.length(); i++)
+			if (sMap.containsKey(s.charAt(i)))
+				sMap.put(s.charAt(i), (sMap.get(s.charAt(i)) + 1));
+			else
+				sMap.put(s.charAt(i), 1);
+		System.out.println(sMap);
+		for (int i = 0; i < t.length(); i++)
+			if (sMap.containsKey(t.charAt(i)))
+				if ((sMap.get(t.charAt(i)) - 1) < 0) {
+					result = false;
+					break;
+				} else
+					sMap.put(t.charAt(i), (sMap.get(t.charAt(i)) - 1));
+
+			else {
+				result = false;
+				break;
+			}
+System.out.println(result);
+	return result;
 	}
 }
